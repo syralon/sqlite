b3a00baea5721035069a69e82f98bc18322f7858
Jan Mercl <0xjnml@gmail.com>
2026-01-13 11:34:46 +0100

    upgrade to SQLite3.51.1

--- doc.go
+++ doc.go
@@ -27,21 +27,21 @@
 //
 //	OS      Arch    SQLite version
 //	------------------------------
-//	darwin	amd64   3.50.4
-//	darwin	arm64   3.50.4
-//	freebsd	amd64   3.50.4
-//	freebsd	arm64   3.50.4
-//	linux	386     3.50.4
-//	linux	amd64   3.50.4
-//	linux	arm     3.50.4
-//	linux	arm64   3.50.4
-//	linux	loong64 3.50.4
-//	linux	ppc64le 3.50.4
-//	linux	riscv64 3.50.4
-//	linux	s390x   3.50.4
-//	windows	386     3.50.4
-//	windows	amd64   3.50.4
-//	windows	arm64   3.50.4
+//	darwin	amd64   3.51.1
+//	darwin	arm64   3.51.1
+//	freebsd	amd64   3.51.1
+//	freebsd	arm64   3.51.1
+//	linux	386     3.51.1
+//	linux	amd64   3.51.1
+//	linux	arm     3.51.1
+//	linux	arm64   3.51.1
+//	linux	loong64 3.51.1
+//	linux	ppc64le 3.51.1
+//	linux	riscv64 3.51.1
+//	linux	s390x   3.51.1
+//	windows	386     3.51.1
+//	windows	amd64   3.51.1
+//	windows	arm64   3.51.1
 //
 // # Benchmarks
 //
@@ -54,6 +54,8 @@
 // https://modern-c.appspot.com/-/builder/?importpath=modernc.org%2fsqlite
 //
 // # Changelog
+//
+//   - 2026-01-13 v1.44.0: Upgrade to SQLite 3.51.1.
 //
 //   - 2025-10-10 v1.39.1: Upgrade to SQLite 3.50.4.
 //
